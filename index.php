aceb2c835375d16cd706edac9802377015779039
MathewsDomingos <mateusdomingos.etec@gmail.com>
2022-08-19 19:43:16 -0300

    Comentarios em PHP

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Programação Web</title>
 </head>
-teste
+
+
+<?php 
+// Comentário em PHP
+/*Comentário em PHP*/
+
+
+?>
+
+
+<p>git --version</p> <br />
+<p>git config --global user.name "MathewsDomingos"</p> <br />
+<p>git config --global user.email "mateusdomingos.etec@gmail.com"</p> <br />
+<p>git init</p> <br /> 
+<p>git add index.php</p> <br />
+<p>git commit -m "First Commit"</p> <br />
+
 <body>
     
 </body>
