a154a9a132b297e1c0ff5c3f589fe06f86225c9a
MathewsDomingos <mateusdomingos.etec@gmail.com>
2022-08-26 19:58:37 -0300

    Array's em php

---
+++ array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+        $carros = array();
+        $carros = array("BMW", "SAVEIRO", "GOL", "PALIO");
+
+        echo "<pre>";
+        print_r($carros);
+        echo "</pre>";
+
+        $nomes = ['FERNANDO', 'ANTONIA', 'MARIA'];
+    ?>
+
+</body>
+</html>
