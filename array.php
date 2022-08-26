32ad63b925a6b2eaa1f8d7ae3b77a8a749f1a29f
MathewsDomingos <mateusdomingos.etec@gmail.com>
2022-08-26 20:01:39 -0300

    array's em php

--- array.php
+++ array.php
@@ -1,22 +1,48 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
     <?php 
-        $carros = array();
+        /*$carros = array();
         $carros = array("BMW", "SAVEIRO", "GOL", "PALIO");
 
         echo "<pre>";
         print_r($carros);
         echo "</pre>";
 
+        $salas = array('lab01' => 40, 'lab02' => 32);
+        $salas['lab03'] = 30;
+
+        echo "<pre>";
+        print_r($salas);
+        echo "</pre>";
+
+        $salas[0] = "24";
+
         $nomes = ['FERNANDO', 'ANTONIA', 'MARIA'];
+        echo "<pre>";
+        print_r($nomes);
+        echo "</pre>";*/
+
+
+        $tabuada = array(0,1,2,3,4);
+        $tabuada[0] = array(0,1,2,3,4);
+        $tabuada[1] = array(0,1,2,3,4);
+        $tabuada[2] = array(0,1,2,3,4);
+        $tabuada[3] = array(0,1,2,3,4);
+        $tabuada[4] = array(0,1,2,3,4);
+
+        echo "<pre>";
+        print_r($tabuada);
+        echo "</pre>";
+
+
     ?>
 
 </body>
